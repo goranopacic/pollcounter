6e6806b1f09e5b60a780fba7b6ac905e341ae979
goranopacic <goran.opacic@esteh.net>
2019-09-21 19:19:44 +0200

    next

--- amplify/backend/function/peopleFunction/src/app.js
+++ amplify/backend/function/peopleFunction/src/app.js
@@ -25,8 +25,8 @@ if(process.env.ENV && process.env.ENV !== "NONE") {
 const userIdPresent = false; // TODO: update in case is required to use that definition
 const partitionKeyName = "uuid";
 const partitionKeyType = "S";
-const sortKeyName = "nickname";
-const sortKeyType = "S";
+const sortKeyName = "";
+const sortKeyType = "";
 const hasSortKey = sortKeyName !== "";
 const path = "/people";
 const UNAUTH = 'UNAUTH';
